662cf83680bedb21f42a7dd6a0d011a34136d3fb
Ali Maredia <alihmaredia@aol.com>
2017-02-25 02:52:06 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+/*WHAT I'M FUCKING TRYING TO DO:
+
+	Set up a server via Express
+	Connecting to our MongoDB database
+	Initializing our socket.io connection
+	Creating our Twitter stream connection */
+
+//Require our dependencies
+
+var express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),		  //templating language that is AWESOME with express
+  mongoose = require('mongoose'), //mongoose is a MongoDB object modeling library
+  twitter = require('ntwitter'),  //ntwitter is a node.js twitter api library
+  routes = require('./routes'),
+  config = require('./config'),
+  streamHandler = require('./utils/streamHandler');
+
+// Create an express instance and set a port variable
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Set handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Disable etag headers on responses
+app.disable('etag');
+
+// Connect to our mongo database
+mongoose.connect('mongodb://localhost/WHATEVER_THE_FUCK_YOU_WANT');
+
+// Create a new ntwitter instance
+var twit = new twitter(config.twitter);
+
+// Index Route
+app.get('/', routes.index);
+
+// Page Route
+app.get('/page/:page/:skip', routes.page);
+
+// Set /public as our static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Fire it up (start our server)
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+// Initialize socket.io
+var io = require('socket.io').listen(server);
+
+// Set a stream listener for tweets matching tracking keywords
+twit.stream('statuses/filter',{ track: 'scotch_io, #scotchio'}, function(stream){
+  streamHandler(stream,io);
+});
